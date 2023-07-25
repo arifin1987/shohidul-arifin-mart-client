a7ea61c0184d7654f16f1f4e64868c8fad98b60e
Shohidul Arifin <shohidularifin@gmail.com>
2023-07-25 07:05:44 -0700

    navbar and footer added

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -3,7 +3,15 @@
 const Footer = () => {
     return (
         <div>
-            <h1>This is Footer</h1>
+            <footer className="footer footer-center p-10 bg-primary text-primary-content">
+                <div>
+                    
+                    
+                    <p>@2023 Assignment of AIDE. Prepared By Shohidul Arifin</p>
+                </div>
+                
+            </footer>
+
         </div>
     );
 };
--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,9 +1,39 @@
+import { Link } from "react-router-dom";
 
 
 const NavBar = () => {
     return (
         <div>
-            <h1>This is NavBar</h1>
+            <div className="navbar bg-base-100">
+  <div className="flex-1">
+    <a className="btn btn-ghost normal-case text-xl text-cyan-600">Shohidul Arifin Mart</a>
+  </div>
+  <div className="flex-none gap-2">
+    <div className="form-control ">
+      <input type="text" placeholder="Search essentials, groceries and more" className="input input-bordered  mr-80 md:w-96 " />
+    </div>
+    <Link className="text-black  " to='/'>Cart</Link>
+    <Link className="text-black  " to='/'>Sign Up/Sign In</Link>
+    <Link className="text-black  " to='/'>Admin</Link>
+    <div className="dropdown dropdown-end">
+      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </label>
+      <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
